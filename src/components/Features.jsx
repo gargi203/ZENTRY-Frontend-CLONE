6f5ed33593c6a50b0617db4433bb865c8fa2a6c4
gargi203 <168561049+gargi203@users.noreply.github.com>
2025-09-14 05:01:35 +0530

    Updated Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -62,7 +62,7 @@ const Features =()=>{
             
             <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
                <BentoCard 
-                    src="public\videos\feature-1.mp4"
+                    src="/videos/feature-1.mp4"
                     title={<>radia<b>n</b>t</>}
                     description="A cross-platform metagame app,turning your 
                 actvities across Web2 and Web3 games into a rewarding adventure."
@@ -71,7 +71,7 @@ const Features =()=>{
             <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
                 <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
                     <BentoCard
-                    src="public\videos\feature-2.mp4"
+                    src="/videos/feature-2.mp4"
                     title={<>Zig<b>m</b>a</>}
                     description="An anime and gaming-inspired NFT collection -the IP primed for expansion"
                     />
@@ -79,7 +79,7 @@ const Features =()=>{
                 </BentoTilt>
                 <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
                     <BentoCard
-                    src="public\videos\feature-3.mp4"
+                    src="/videos/feature-3.mp4"
                     title={<>N<b>E</b>XUS</>}
                     description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
                     />
@@ -87,7 +87,7 @@ const Features =()=>{
                 </BentoTilt>
                 <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
                     <BentoCard
-                    src="public\videos\feature-4.mp4"
+                    src="/videos/feature-4.mp4"
                     title={<>az<b>u</b>l</>}
                     description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
                     />
@@ -103,7 +103,7 @@ const Features =()=>{
                 </BentoTilt>
                 <BentoTilt className="bento-tilt_2">
                     <video
-                        src="public\videos\feature-5.mp4"
+                        src="/videos/feature-5.mp4"
                         loop 
                         muted 
                         autoPlay 
