a0ccae4f9ff95a2d71ff7b856e7658cad19969cc
gargi203 <168561049+gargi203@users.noreply.github.com>
2025-09-17 01:07:52 +0530

    Update Story.jsx

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -71,7 +71,7 @@ const Story=()=>{
                     </div>
 
                 </div>
-                <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+                <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end relative z-20">
                     <div className="flex h-full w-fit flex-col items-center md:items-start ">
                         <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
                              Where realms converge, lies Zentry and the boundless pillar.
